201da45efe8c53726a570eba95e1363224ff7217
Juraj Uhlar <juro.uhlar@gmail.com>
2025-03-08 09:32:38 +0100

    simple fpjs demo, just console.log

--- fpjs_demo/lib/main.dart
+++ fpjs_demo/lib/main.dart
@@ -1,15 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:fpjs_pro_plugin/error.dart' show FingerprintProError;
+import 'package:fpjs_pro_plugin/fpjs_pro_plugin.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  void initState() {
+    super.initState();
+    initFingerprint();
+  }
+
+  void initFingerprint() async {
+    try {
+      await FpjsProPlugin.initFpjs('eajUlf6axysf2z89ZVWx');
+      identify();
+    } on FingerprintProError catch (e) {
+      print(e);
+    }
+  }
+
+  void identify() async {
+    try {
+      var visitorId = await FpjsProPlugin.getVisitorId() ?? 'Unknown';
+      print(visitorId);
+      // use the visitor id
+    } on FingerprintProError catch (e) {
+      print(e);
+      // process an error somehow
+      // check lib/error.dart to get more info about error types
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
           child: Text('Hello Fpjs World!'),
